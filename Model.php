9d87f75d2d4be358a0ba44e288fbe70ac1a0cbd9
Tran Thien Nhan <nhan_tt@vietvang.net>
2026-05-12 23:24:33 +0700

    v0.23.1: Fix PHPStan return.type for Model::create()
    Replace self::createInstance() with new static() in create() so
    PHPStan correctly infers the return type as static(Model).
    Update baseline count for new.static from 2 to 3.

--- Model.php
+++ Model.php
@@ -249,7 +249,7 @@ abstract class Model implements \JsonSerializable, \ArrayAccess
     /** @param array<string, mixed> $attributes */
     public static function create(array $attributes): static
     {
-        $instance = self::createInstance();
+        $instance = new static();
         $instance->fill($attributes);
         $instance->save();
 
