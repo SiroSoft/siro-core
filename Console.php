1c7054a7f0640ec65ca5a79fca32bc59b2547cf6
Tran Thien Nhan <nhan_tt@vietvang.net>
2026-05-29 18:17:28 +0700

    chore: bump version to 0.32.0

--- Console.php
+++ Console.php
@@ -91,7 +91,7 @@ use Siro\Core\Commands\TinkerCommand;
 
 final class Console
 {
-    public const VERSION = '0.31.0';
+    public const VERSION = '0.32.0';
 
     /** @var array<string, array{handler: class-string, desc: string, usage: string}> */
     private static array $appCommands = [];
